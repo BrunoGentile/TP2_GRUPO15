f096ced6d1218e637239efaaf0a590b4ae40c5d8
NicolasPiccioni <Nicolas_Piccioni@hotmail.com>
2025-04-12 20:32:27 -0300

    Ejercicio 5: Calculo de precio

--- TP2_GRUPO_15/Ejercicio5.aspx.cs
+++ TP2_GRUPO_15/Ejercicio5.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,35 @@ namespace TP2_GRUPO_15
 {
     public partial class Ejercicio5 : System.Web.UI.Page
     {
+
+        protected float CalcularPrecioTotal() /// RETORNA PRECIO TOTAL
+        {
+            float PrecioTotal = 0; /// VARIABLE QUE ALMACENA PRECIO TOTAL
+
+            switch (ddlMemoria.SelectedIndex) /// ALMACENA EL PRECIO DE LA MEMORIA
+            {
+                case 0: PrecioTotal += 200; break;
+                case 1: PrecioTotal += 375; break;
+                case 2: PrecioTotal += 500; break;
+            }
+
+            foreach ( ListItem Item in cblAccesorios.Items ) /// RECORRE LOS ACCESORIOS
+            {
+                if ( Item.Selected ) /// VERIFICA QUE EL ITEM ESTÉ SELECCIONADO
+                {
+
+                    switch( int.Parse(Item.Value) ){
+                       case 0: PrecioTotal += 2000.50f; break;
+                       case 1: PrecioTotal += 550.50f; break;
+                       case 2: PrecioTotal += 1200; break;
+                    }
+
+                }
+            }
+
+            return PrecioTotal;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //CARGAR DROPDOWNLIST
@@ -21,6 +51,13 @@ namespace TP2_GRUPO_15
         protected void btnInicio_Click(object sender, EventArgs e)
         {
             Response.Redirect("Inicio.aspx");
+        }
+
+        protected void btnCalcularPrecio_Click(object sender, EventArgs e)
+        {
+            
+            float PrecioTotal = CalcularPrecioTotal();
+
         }
     }
 }
--- TP2_GRUPO_15/Ejercicio5.aspx.designer.cs
+++ TP2_GRUPO_15/Ejercicio5.aspx.designer.cs
@@ -51,15 +51,6 @@ namespace TP2_GRUPO_15
         protected global::System.Web.UI.WebControls.Button btnCalcularPrecio;
 
         /// <summary>
-        /// Control btnReset.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnReset;
-
-        /// <summary>
         /// Control btnInicio.
         /// </summary>
         /// <remarks>
